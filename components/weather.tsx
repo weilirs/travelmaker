1b81459c1ebc399c584495aaa0d7db073a0fcf37
weilirs <weilirs@umich.edu>
2024-05-05 15:55:58 -0400

    feat: weather widget

--- components/weather.tsx
+++ components/weather.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { useLocations } from "@/utils/locationContext";
+import Image from "next/image";
 
 export default function Weather({ onSunrise, onSunset }) {
   const [date, setDate] = useState("");
@@ -33,6 +34,18 @@ export default function Weather({ onSunrise, onSunset }) {
     }
   };
 
+  function getImageSrc(conditionText) {
+    if (conditionText.toLowerCase().includes("rain")) {
+      return "../static/weather_icons/animated/rainy-5.svg"; // Path to rainy weather icon
+    } else if (conditionText.toLowerCase().includes("cloud")) {
+      return "../static/weather_icons/animated/cloudy-day-1.svg"; // Path to cloudy weather icon
+    } else if (conditionText.toLowerCase().includes("snow")) {
+      return "../static/weather_icons/animated/snowy-1.svg"; // Path to sunny weather icon
+    } else {
+      return "../static/weather_icons/animated/day.svg"; // Default weather icon
+    }
+  }
+
   return (
     <div>
       <h2>Enter Date for Weather Forecast</h2>
@@ -53,12 +66,43 @@ export default function Weather({ onSunrise, onSunset }) {
       </form>
       {forecast && (
         <div>
-          <h3>Forecast for {forecast.date}</h3>
-          <p>Max Temperature: {forecast.day.maxtemp_c}°C</p>
-          <p>Min Temperature: {forecast.day.mintemp_c}°C</p>
-          <p>Condition: {forecast.day.condition.text}</p>
-          <p>Sunrise: {forecast.astro.sunrise}</p>
-          <p>Sunset: {forecast.astro.sunset}</p>
+          {/* <div>
+            <p>Max Temperature: {forecast.day.maxtemp_c}°C</p>
+            <p>Min Temperature: {forecast.day.mintemp_c}°C</p>
+            <p>Condition: {forecast.day.condition.text}</p>
+            <p>Sunrise: {forecast.astro.sunrise}</p>
+            <p>Sunset: {forecast.astro.sunset}</p>
+          </div> */}
+          <div className="relative flex justify-center items-center mt-4">
+            <div className=" relative w-220 h-250 flex flex-col items-center justify-between py-5 px-2.5 bg-custom-gray rounded-md bg-opacity-80">
+              <p className="font-bold text-sm tracking-wide text-white">
+                {city.name}
+              </p>
+              <p className="font-medium text-custom-size tracking-custom-spacing">
+                {forecast.day.condition.text}
+              </p>
+              <Image
+                src={getImageSrc(forecast.day.condition.text)}
+                alt="weather icon"
+                width={100}
+                height={100}
+              ></Image>
+              <div className="w-full flex items-center justify-between">
+                <div className="w-1/2 flex flex-col items-end justify-center px-5 py-0 gap-1 ">
+                  <p className="text-sm font-semibold">Min</p>
+                  <p className="text-xs font-medium">
+                    {forecast.day.mintemp_c}°C
+                  </p>
+                </div>
+                <div className="w-1/2 flex flex-col items-start justify-centter px-5 py-0 gap-1 border-l-2 border-white">
+                  <p className="text-sm font-semibold">Max</p>
+                  <p className="text-xs font-medium">
+                    {forecast.day.maxtemp_c}°C
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,24 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "custom-gray": "rgba(65, 65, 65, 0.308)",
+      },
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        "custom-gray": "rgb(248, 242, 242)",
+      }),
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       width: {
-        240: "48rem",
+        240: "48rem", // TODO: use pixel
+        "220": "220px",
+      },
+      height: {
+        "250": "250px", // Custom height 250px
       },
     },
   },
